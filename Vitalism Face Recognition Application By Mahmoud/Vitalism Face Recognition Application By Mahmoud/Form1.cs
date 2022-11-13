b7cc73f239cb2268cfaacd78006f667493b816e8
MahmoudAlyosify <mahmoudalyosify@gmail.com>
2022-11-13 15:30:20 +0200

    Create C# project

---
+++ Vitalism Face Recognition Application By Mahmoud/Vitalism Face Recognition Application By Mahmoud/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vitalism_Face_Recognition_Application_By_Mahmoud
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Vitalism Face Recognition Application By Mahmoud/Vitalism Face Recognition Application By Mahmoud/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vitalism_Face_Recognition_Application_By_Mahmoud
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
